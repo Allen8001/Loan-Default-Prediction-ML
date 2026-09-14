463788c2909ff4a7a9e6321c5f5c9d56867abbd0
Allen Wang <allenwang8001@gmail.com>
2026-09-14 20:06:24 +1200

    chore: remove legacy project structure

--- loan-dufault-predition/launch_fixed_interface.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Fixed Interface App
-echo =============================
-streamlit run app/app_real_data.py --server.port=8503
-pause
--- loan-dufault-predition/launch_simple.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Simple Loan App
-echo =========================
-streamlit run app/app_real_data.py --server.port=8501
-pause
